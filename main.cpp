be1f69869f38ba41220e4cb4451440dbe19272d4
AlexeyKulmetev <44574285+AlexeyKulmetev@users.noreply.github.com>
2023-02-06 11:04:30 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "TBox.h"
+
+int main() {
+	TBox mainBox = TBox();
+	float tmp;
+
+	std::cout << "Enter length of the main box: ";
+	std::cin >> tmp;
+	mainBox.setLenght(tmp);
+	std::cout << "Enter width of the main box: ";
+	std::cin >> tmp;
+	mainBox.setWidth(tmp);
+	std::cout << "Enter height of the main box: ";
+	std::cin >> tmp;
+	mainBox.setHeight(tmp);
+
+	std::cout << std::endl;
+	mainBox.print();
+	std::cout << std::endl;
+
+	TBox smallerBox = TBox();
+	std::cout << "Enter length of the smaller box: ";
+	std::cin >> tmp;
+	smallerBox.setLenght(tmp);
+	std::cout << "Enter width of the smaller box: ";
+	std::cin >> tmp;
+	smallerBox.setWidth(tmp);
+	std::cout << "Enter height of the smaller box: ";
+	std::cin >> tmp;
+	smallerBox.setHeight(tmp);
+
+	std::cout << std::endl;
+	std::cout << "The amount of smaller boxes, that will fitt in the main box = " <<
+		mainBox.box_fit(smallerBox);
+	return 0;
+}
